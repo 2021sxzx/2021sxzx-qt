cbcf57c3d9ed2098685e72d15b1ac9ff4b725d4b
UmyNya <1079102275@qq.com>
2022-07-28 15:20:51 +0800

    refactor(SlideCS): 调整了右下角智能客服的悬浮窗的样式

--- src/views/components/SlideCS/SlideCS.js
+++ src/views/components/SlideCS/SlideCS.js
@@ -1,21 +1,23 @@
 import React, {useState} from 'react'
 import style from './SlideCS.module.scss'
-import { Link } from 'react-router-dom'
-import { Images } from '../../../assets'
+import {Link} from 'react-router-dom'
+import {Images} from '../../../assets'
 
 export default function SlideCS() {
     const [isSiderHide, setIsSiderHide] = useState(false);
     return (
         <div className={isSiderHide ? style.hide : null}>
             <div className={style.container}>
-                <img src={Images.common.icDelete} 
-                    className={style.delete} 
-                    onClick={setIsSiderHide.bind(this, true)}/>
+                <img src={Images.common.icDelete}
+                     className={style.delete}
+                     onClick={setIsSiderHide.bind(this, true)}
+                 alt={'关闭'}/>
                 <Link to='/intelligentCS'>
                     <div className={style.clickBox}>
-                        <img src={Images.common.icZNKF}/>
+                        <img src={Images.common.icZNKF} alt={'智能客服'}/>
                     </div>
                 </Link>
             </div>
         </div>
-    )}
+    )
+}
