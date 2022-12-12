40a5d12a3df8108e976164e1f85a3de0b6c4a400
UmyNya <1079102275@qq.com>
2022-12-12 22:28:41 +0800

    perf: 调整首页性能，使用懒加载和代码分割技术优化

--- src/router/IndexRouter.js
+++ src/router/IndexRouter.js
@@ -1,21 +1,44 @@
-import React from 'react'
+import React, {Suspense, lazy} from 'react'
 import {HashRouter, Route, Switch} from 'react-router-dom'
-import Login from '../views/LoginPage/Login.js'
-import GuideResult from '../views/GuideResult/GuideResult'
-import SearchPage from '../views/SearchPage/SearchPage.js'
-import Home from '../views/HomePage/HomePage.js'
-import Navigation from '../views/Navigation/Navigation.js'
+// import Login from '../views/LoginPage/Login.js'
+// import GuideResult from '../views/GuideResult/GuideResult'
+// import SearchPage from '../views/SearchPage/SearchPage.js'
+// import Home from '../views/HomePage/HomePage.js'
+// import Navigation from '../views/Navigation/Navigation.js'
+
+// 懒加载和代码分割
+const Login = lazy(() => {
+    return import('../views/LoginPage/Login.js')
+})
+
+const GuideResult = lazy(() => {
+    return import('../views/GuideResult/GuideResult')
+})
+
+const SearchPage = lazy(() => {
+    return import('../views/SearchPage/SearchPage.js')
+})
+
+const Home = lazy(() => {
+    return import('../views/HomePage/HomePage.js')
+})
+
+const Navigation = lazy(() => {
+    return import('../views/Navigation/Navigation.js')
+})
 
 export default function IndexRouter() {
     return (
         <HashRouter>
             <Switch>
-                <Route path='/login' component={Login}/>
-                <Route path='/v1/taskResult' component={GuideResult}/>
-                <Route path='/searchPage' component={SearchPage}/>
-                <Route path='/home' component={Home}/>
-                <Route path='/navigation' component={Navigation}/>
-                <Route path='/' component={Home}/>
+                <Suspense fallback={<div>正在加载中，请稍后...</div>}>
+                    <Route path='/login' component={Login}/>
+                    <Route path='/v1/taskResult' component={GuideResult}/>
+                    <Route path='/searchPage' component={SearchPage}/>
+                    <Route path='/home' component={Home}/>
+                    <Route path='/navigation' component={Navigation}/>
+                    <Route path='/' component={Home}/>
+                </Suspense>
             </Switch>
         </HashRouter>
 
